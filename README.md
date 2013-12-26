1aed35fa4a0cd01328df0e7973e2aa421aa72ab6
Jeshua Maxey <me@jeshua.co>
2013-12-26 13:14:42 +0000

    made button styles consistent. Implemented an app.snippets object to store text do prevent multiple identical api calls. Made title box in context box a link to releveant wikipedia article. Context box shows/hides with slide up/down animatio

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Context
 
 A neat Chrome plugin to provide context when browsing [wikipedia](http://wikipedia.org) built with the help of [extensionizr](http://extensionizr.com).
+
+## To Do
+
+* ensure first sentence is shown every time
+* account for links that start with '/wiki/Help:something' (see pronuciation guides in first line of first para)
+* stop the context box resizing when it shows 'loading...'
+* fail gracefully
+* make an icon
+* change the title of context box to title of wikipedia article
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,23 +1,30 @@
 var app = app || {};
 
+//remebers state of the app
+app.contextOn = true;
+
+//stores all data from api calls to prevent making same call twice
+//consider using local database here
+app.snippets = {};
+
 app.contextBox = "<button id='showContext' class='btn showContext'>Context</button>" +
 									"<div id='contextBoxWrapper' class='contextBoxWrapper'>" +
 										"<div id='title' class='title'>" +
-											"<h1>Context</h1>" +
+											"<h1><a href='http://jeshua.co'>Context</a></h1>" +
 											"<button id='hideContext' class='btn hideContext'>Hide</button>" +
 										"</div>" +
 										"<div id='contextBox' class='hidden'></div>" +
 									"</div>";
 
-app.loadingGif = "<p>Loading...</p>";//"<img src='img/loading.gif'/>";
+//shown when making API calls
+app.loadingGif = "<p>Loading...</p>";
 
-app.contextOn = true;
-
+//run on page load
 app.main = function() {
 	//add classes to links suitable for creating tooltips for for efficiency
 	app.scanSuitableLinks();
 	//put the context UI in the DOM
-	app.addContextBox();
+	$("body").append(app.contextBox)
 	//add event listeners to turn context on/off
 	$('#hideContext').on('click', app.hideContext);
 	$('#showContext').on('click', app.showContext);
@@ -31,53 +38,66 @@ app.scanSuitableLinks = function() {
 	$('#mw-content-text p a').each(function(i, a) {
 		app.links.push(a);
 	});
-	//console.log(app.links);
 }
 
+//when context is turned off
 app.hideContext = function() {
 	app.contextOn = !app.contextOn;
-	$('#contextBoxWrapper').hide();
+	$('#contextBoxWrapper').slideUp(200);
 }
 
+//when context is turned on
 app.showContext = function() {
 	app.contextOn = !app.contextOn;
-	$('#contextBoxWrapper').show();
+	$('#contextBoxWrapper').slideDown(200);
 }
 
-app.addContextBox = function() {
-	$("body").append(app.contextBox)
-}
-
+//initiates context search
 app.getContext = function() {
 	//show a loading gif in the context box
 	$('#contextBox').html(app.loadingGif);
-	//get the data
-	var page = this.getAttribute("href").substring(6); //substring(6) removes /wiki/ from url
-	app.getData(page);
+	//isolate the page name
+	page = this.getAttribute("href").substring(6); //substring(6) removes /wiki/ from url
+	//use the array if it has the relevant snippet
+	if(app.snippets[page] !== undefined) {
+		app.populateContextBox(page)
+	} else {
+		//get the data
+		app.getData(page);
+	}
 }
 
 //make api call to wikipedia
 app.getData = function(page) {
 	if(app.contextOn) {
-	var url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&prop=text&page=" + page;
-	$.ajax({
-		url: url,
-	}).done(app.populateContextBox);
+		var url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&prop=text&page=" + page;
+		$.ajax({
+			url: url,
+		}).done(function(data) {
+			app.processData(data, page);
+		});
 	}
 }
 
-//
-app.populateContextBox = function(data) {
-	console.log('populate');
+//save data into app.snippets
+app.processData = function(data, page) {
+	console.log('process');
 	app.fullText = data.parse.text["*"];
 	app.text = [];
 	app.fullText.replace(/<p>(.*?)<\/p>/g, function () {
-		app.text.push(arguments[1]);
-		console.log(app.text);
-		$('#contextBox').html(app.text).removeClass('hidden');
+		app.snippets[page] = {};
+		app.snippets[page].url = "/wiki/" + page;
+		app.snippets[page].title = page.replace(/_/g, " ");	//replace underscores with spaces
+		app.snippets[page].text = arguments;
+		app.populateContextBox(page);
 	});
-	var title = $($('#contextBox b')[0]).html();
-	$('#title h1').html(title);
+}
+
+//show context snippet
+app.populateContextBox = function(page) {
+	$('#contextBox').html(app.snippets[page].text[0]).removeClass('hidden');
+	$('#title h1 a').attr('href', app.snippets[page].url);
+	$('#title h1 a').html(app.snippets[page].title);
 }
 
 //must go last
