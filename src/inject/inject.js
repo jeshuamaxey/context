e649e4567f8a1fcd58eae292bb2b24da99766da2
Jeshua Maxey <me@jeshua.co>
2013-12-28 14:26:02 +0000

    cleaned out commented resize code with a view to re add it in a development branch

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -15,16 +15,13 @@ app.linkSelector = "#mw-content-text p a.contextEligible";
 app.contexts = {};
 
 app.contextBox = 	"<button id='showContext' class='btn showContext'>Context</button>" +
-									//"<div id='contextBoxWrapperOuter' class='contextBoxWrapperOuter'>" +
 										"<div id='contextBoxWrapperInner' class='contextBoxWrapperInner'>" +
-											//"<div class='ui-resizable-handle ui-resizable-n' id='ngrip'></div>" +
 											"<div id='title' class='title'>" +
 												"<h2><a href='http://jeshua.co'>Context</a></h2>" +
 												"<button id='hideContext' class='btn hideContext'>Hide</button>" +
 											"</div>" +
 											"<div id='contextBox' class='hidden'></div>" +
-										"</div>" //+
-									//"</div>";
+										"</div>";
 
 //shown when making API calls
 app.loadingGif = "<p>Loading...</p>";
@@ -35,8 +32,6 @@ app.main = function() {
 	app.scanSuitableLinks();
 	//put the context UI in the DOM
 	$("body").append(app.contextBox)
-	//$('#contextBoxWrapperOuter').draggable();
-	//$('#contextBoxWrapperInner').resizable( {handles: {'n': '#ngrip'} } );
 	//add event listeners to turn context on/off
 	$('#hideContext').on('click', app.contextOff);
 	$('#showContext').on('click', app.contextOn);
